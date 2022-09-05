a1f3948c443cb92990e50ce7f04aa0d7781fe9b1
Melissa Casole <melissacasole@Melissas-MBP.lan>
2022-09-05 19:03:59 -0400

    adding new config file

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+// import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '/melissacasole8/'
+
+
+    // to build multi pages
+    // build: {
+    //     rollupOptions: {
+    //         input: {
+    //             main: resolve(__dirname, 'index.html'),
+    //             nested: resolve(__dirname, 'nested/index.html')
+    //         }
+    //     }
+    // }
+
+})
